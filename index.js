28dd4386af9c774f88d3361c55aeea1e10bd43f9
JuanIgnacioAmarilloJacob <amarillojuanignacio8@gmail.com>
2022-09-07 14:06:59 -0300

    Delete con postman

--- index.js
+++ index.js
@@ -62,41 +62,22 @@ const empresas = [
 ]
 console*/
 
-/*const jugadores = [
-    { ID: "1", clase: "mago", nivel: 35, inventario:
-    ["Manzana", "Poción de maná", "Vara mágica"] },
-    { ID: "2", clase: "ladron", nivel: 65, inventario:
-    ["Daga", "Katana de fuego", "Poción de vida"] },
-    { ID: "4", clase: "curandero", nivel: 73, inventario:
-    ["Vara mágica", "Armadura ligera"] },
-    { ID: "3", clase: "espadachin", nivel: 36, inventario:
-    ["Casco de hierro", "Katana de fuego", "Pocíón de velocidad"] },
-    { ID: "5", clase: "mago", nivel: 26, inventario:
-    ["Carta de PecoPeco", "Oridecon", "Poción de concentración"] },
-]
-
-const sospechosos = jugadores.filter(item => item.inventario
-    .some(elementoInventario => elementoInventario === "Katana de fuego")
-).map(elemento => elemento.ID);
-
-console.log(sospechosos);*/
-
 const { json } = require('body-parser');
 const { profileEnd } = require('console');
 const express = require('express');
 const fs = require('fs');
 const products = require('./data/MOCK_DATA.json')
 const app = express();
-
 const PORT = 3000;
 
-console.log(products);
+//console.log(products);
 
 app.use(express.json());
 
 // Envío un mensaje al server
+// El 200 indica que está succes con el servidor
 app.get('/',(req, res)=>{
-res.send("Hola mundo");
+res.send(200, "Bienvenido al servidor");
 })
 
 //Dato que viene del servidor, por eso trabajo con el req
@@ -127,6 +108,7 @@ app.post('/products/add', (req, res) =>{
         return res.status(500).json({mensaje: "Este producto ya existe"});
     }
     products.push(newProducts);
+    //Herramienta para escribir archivos (file system)
     fs.writeFile('./data/MOCK_DATA.json', JSON.stringify(products), (error)=>{
         if(error){
             res.status(500).json({mensaje: "Internal Error"});
@@ -135,9 +117,42 @@ app.post('/products/add', (req, res) =>{
     res.json(newProducts);
 })
 
-//app.delete('products/delete/:id')
+app.delete('/products/delete/:id', (req, res) => {
+    const id = req.params.id;
+    const filteredProducts = products.filter(product => product.id == id)
 
-//Indico el puerto 
+    if(filteredProducts.length === 0){
+        return res.status(204).json({message: "Producto no encontrado"});
+    }
+    //En que número está ese elemento
+    const index = products.indexOf(filteredProducts[0]);
+    //A partir del index
+    products.splice(index, 1);
+    fs.writeFile("./data/MOCK_DATA.json", JSON.stringify(products), (error) => {
+        if(error) res.status(500).json({message: 'internal error'})
+    })
+    res.status(200).json({message: "Product deleted"});
+})
+
+/*app.delete('/products/delete/:name', (req, res) => {
+    const productName = req.params.name;
+    const filteredProducts = products.filter(product => product.name.includes(productName));
+
+    if(filteredProducts.length === 0){
+        return res.status(204).json({message: "Producto no encontrado"});
+    }
+    //En que número está ese elemento
+    const index = products.indexOf(filteredProducts[0]);
+    //A partir de la posicion del arreglo borra la cantidad de elementos que se le pase.
+    products.splice(index, 1);
+
+    fs.writeFile("./data/MOCK_DATA.json", JSON.stringify(products), (error) => {
+        if(error) res.status(500).json({message: 'internal error'})
+    })
+    res.status(200).json({message: "Product deleted"});
+})*/
+
+//Arranca nuestro servidor api
 app.listen(PORT, () =>{
     console.log('OK');
 })
